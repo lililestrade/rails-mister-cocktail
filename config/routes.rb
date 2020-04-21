b3ef288ae0281c055492ea7b6c3e37d8b35f03d8
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-04-21 19:28:23 +0200

    working alriiight

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root      'home#index'
+  resources :home, only: [:index, :new, :create]
+
   # resources :cocktails, only: [:index, :show, :new, :create] do
   #   resources :doses, only: %w[new create destroy], shallow: true
   # end
-  root      'home#index'
-  resources :home, only: [:index, :new, :create]
 
   # root to: 'cocktails#index', as: '/'
   # root to: 'cocktails#index';
