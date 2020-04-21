db0dec3a75a31e9b1e21470faff1eeb389f837aa
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-04-21 19:31:56 +0200

    remis comme avant

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root      'home#index'
-  resources :home, only: [:index, :new, :create]
+  # root      'home#index'
+  # resources :home, only: [:index, :new, :create]
 
-  # resources :cocktails, only: [:index, :show, :new, :create] do
-  #   resources :doses, only: %w[new create destroy], shallow: true
-  # end
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: %w[new create destroy], shallow: true
+  end
 
-  # root to: 'cocktails#index', as: '/'
-  # root to: 'cocktails#index';
+  root to: 'cocktails#index', as: '/'
+  root to: 'cocktails#index';
 end
